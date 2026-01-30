5a61d267ccb7ae665d7584eec77c0bf7f6a619ea
Yash-Raj-5424 <myash3499@gmail.com>
2026-01-30 23:55:40 +0530

    First commit

---
+++ tensor/src/main/java/com/eigen/tensor/TensorApplication.java
@@ -0,0 +1,13 @@
+package com.eigen.tensor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TensorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TensorApplication.class, args);
+	}
+
+}
